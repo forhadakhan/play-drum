aaf44222424c4e1ec7e1bc28c2feac44f111a38f
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-06 22:11:56 +0600

    Keyboard control added and working

--- src/components/DrumMachine.jsx
+++ src/components/DrumMachine.jsx
@@ -2,11 +2,31 @@ import React, { useEffect } from 'react';
 
 const DrumMachine = () => {
 
+    // Add the event listener for key press
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyPress);
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+        };
+    }, []);
+
     const handleClick = (event) => {
         const audio = event.target.querySelector('audio');
         if (audio) {
             audio.currentTime = 0;
             audio.play();
+        }
+    };
+    
+    const handleKeyPress = (event) => {
+        const key = event.key.toUpperCase();
+        const drumPad = document.getElementById(key);
+        if (drumPad) {
+            const audio = drumPad.querySelector('audio');
+            if (audio) {
+                audio.currentTime = 0;
+                audio.play();
+            }
         }
     };
 
