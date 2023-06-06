ecc039cd2aed380530fda8d13aa31c039e04750d
FK <67508944+forhadakhan@users.noreply.github.com>
2023-06-07 02:26:33 +0600

    Feature Added: Power Switch  & Bank Switch

--- src/components/DrumMachine.jsx
+++ src/components/DrumMachine.jsx
@@ -1,110 +1,158 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const DrumMachine = () => {
-    const str = [null, 'Heater 1', 'Heater 2', 'Heater 3', 'Heater 4', 'Clap', 'Open HH', 'Kick n\' Hat', 'Kick', 'Close HH']
+    const str1 = [null, 'Heater 1', 'Heater 2', 'Heater 3', 'Heater 4', 'Clap', 'Open HH', 'Kick n\' Hat', 'Kick', 'Close HH']
+    const str2 = [null, 'Chord 1', 'Chord 2', 'Chord 3', 'Shaker', 'Open HH', 'Closed HH', 'Punchy Kick', 'Side Stick', 'Snare']
+    const audio1 = [null, 'audio-q', 'audio-w', 'audio-e', 'audio-a', 'audio-s', 'audio-d', 'audio-z', 'audio-x', 'audio-c']
+    const audio2 = [null, 'Chord_1', 'Chord_2', 'Chord_3', 'Give_us_a_light', 'Dry_Ohh', 'Bld_H1', 'punchy_kick_1', 'side_stick_1', 'Brk_Snr']
 
-    // Add the event listener for key press
+    const [powerSwitchChecked, setPowerSwitchChecked] = useState(true);
+    const [bankSwitchChecked, setBankSwitchChecked] = useState(false);
+
+    const handlePowerChange = () => {
+        setPowerSwitchChecked(!powerSwitchChecked);
+        if (powerSwitchChecked) {
+            updateDisplay('Power Off')
+        } else {
+            updateDisplay('Power On')
+        }
+    };
+
+    const handleBankChange = () => {
+        setBankSwitchChecked(!bankSwitchChecked);
+        if (bankSwitchChecked) {
+            updateDisplay('Bank Down')
+        } else {
+            updateDisplay('Bank Up')
+        }
+    };
+
+    // the event listener for key press
     useEffect(() => {
         window.addEventListener('keydown', handleKeyPress);
         return () => {
             window.removeEventListener('keydown', handleKeyPress);
         };
-    }, []);
+    }, [powerSwitchChecked, bankSwitchChecked]);
 
     const handleKeyPress = (event) => {
-        const key = event.key.toUpperCase();
-        const drumPad = document.getElementById(key);
-        if (drumPad) {
-            const audio = drumPad.querySelector('audio');
-            const audioId = audio.getAttribute('id');
+        if (powerSwitchChecked) {
+            const key = event.key.toUpperCase();
+            const drumPad = document.getElementById(key);
+            if (drumPad) {
+                const audio = drumPad.querySelector('audio');
+                const audioId = audio.getAttribute('id');
+                console.log(audioId);
+                if (audio) {
+                    audio.currentTime = 0;
+                    audio.play();
+                    updateDisplay(bankSwitchChecked ? str2[audioId] : str1[audioId]);
+                }
+            }
+        };
+    };
+
+        const audioSource = (key) => {
+            if (bankSwitchChecked) {
+                return `src/assets/audio/${audio2[key]}.mp3`;
+            } else {
+                return `src/assets/audio/${audio1[key]}.mp3`;
+            }
+        };
+
+        const handleClick = (event) => {
+            const audio = event.currentTarget.querySelector('audio');
             if (audio) {
                 audio.currentTime = 0;
                 audio.play();
-                updateDisplay(str[audioId]);
+                const audioId = audio.getAttribute('id');
+                updateDisplay(bankSwitchChecked ? str2[audioId] : str1[audioId]);
             }
-        }
-    };
-
-    const audio1 = [null, 'audio-q', 'audio-w', 'audio-e', 'audio-a', 'audio-s', 'audio-d', 'audio-z', 'audio-x', 'audio-c']
-    const audio2 = [null, 'Cloud_1', 'Cloud_2', 'Cloud_3', 'Give_us_a_light', 'Dry_Ohh', 'Bld_H1', 'punchy_kick_1', 'side_stick_1', 'Brk_Snr']
-
-    const audioSource = (key) => {
-        return `src/assets/audio/${audio1[key]}.mp3`;
-    };
-
-    const handleClick = (event) => {
-        const audio = event.currentTarget.querySelector('audio');
-        if (audio) {
-            audio.currentTime = 0;
-            audio.play();
-            const audioId = audio.getAttribute('id');
-            updateDisplay(str[audioId]);
-        }
-    };
+        };
 
 
-    const updateDisplay = (text) => {
-        const display = document.getElementById('display');
-        if (display) {
-            display.innerText = text;
-        }
-    };
+        const updateDisplay = (text) => {
+            const display = document.getElementById('display');
+            if (display) {
+                display.innerText = text;
+            }
+        };
 
-    return (
-        <div id="drum-machine" className="container">
-            <div className='d-flex flex-column align-items-center'>
-                <div className='d-flex align-items-center justify-content-center bg-white p-2 border rounded-3 pad-display' id='string-display'>
-                    <div>  <i className="bi bi-music-note">&nbsp;</i></div>
-                    <div id="display"></div>
-                </div>
-                <div className='pad-bank' id="pad">
-                    <div className="row">
-                        <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="Q">
-                            <span className='fs-1'>Q</span>
-                            <audio className="clip" id="1" src={audioSource(1)}></audio>
-                        </div>
-                        <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="W">
-                            <span className='fs-1'>W</span>
-                            <audio className="clip" id="2" src={audioSource(2)}></audio>
-                        </div>
-                        <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="E">
-                            <span className='fs-1'>E</span>
-                            <audio className="clip" id="3" src={audioSource(3)}></audio>
+        return (
+            <div id="drum-machine" className="container">
+                <div className='d-flex flex-column align-items-center'>
+                    <div className="container pad-display">
+                        <div className="row ">
+                            <div className="col bg-white m-4 p-2 border rounded-3">
+                                <div className="form-check form-switch">
+                                    <input className="form-check-input" type="checkbox" id="power" checked={powerSwitchChecked} onChange={handlePowerChange} />
+                                    <label className="form-check-label" htmlFor="power">
+                                        Power
+                                    </label>
+                                </div>
+                            </div>
+                            <div className="col bg-white m-4 p-2 border rounded-3">
+                                <div className="form-check form-switch">
+                                    <input className="form-check-input" type="checkbox" id="bank" checked={bankSwitchChecked} onChange={handleBankChange} />
+                                    <label className="form-check-label" htmlFor="bank">
+                                        Bank
+                                    </label>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="A">
-                            <span className='fs-1'>A</span>
-                            <audio className="clip" id="4" src={audioSource(4)}></audio>
+                    <div className='d-flex align-items-center justify-content-center bg-white p-2 border rounded-3 pad-display' id='string-display'>
+                        <div>  <i className="bi bi-music-note">&nbsp;</i></div>
+                        <div id="display"></div>
+                    </div>
+                    <div className='pad-bank' id="pad">
+                        <div className="row">
+                            <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="Q">
+                                <span className='fs-1'>Q</span>
+                                <audio className="clip" id="1" src={audioSource(1)}></audio>
+                            </div>
+                            <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="W">
+                                <span className='fs-1'>W</span>
+                                <audio className="clip" id="2" src={audioSource(2)}></audio>
+                            </div>
+                            <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="E">
+                                <span className='fs-1'>E</span>
+                                <audio className="clip" id="3" src={audioSource(3)}></audio>
+                            </div>
                         </div>
-                        <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="S">
-                            <span className='fs-1'>S</span>
-                            <audio className="clip" id="5" src={audioSource(5)}></audio>
+                        <div className="row">
+                            <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="A">
+                                <span className='fs-1'>A</span>
+                                <audio className="clip" id="4" src={audioSource(4)}></audio>
+                            </div>
+                            <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="S">
+                                <span className='fs-1'>S</span>
+                                <audio className="clip" id="5" src={audioSource(5)}></audio>
+                            </div>
+                            <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="D">
+                                <span className='fs-1'>D</span>
+                                <audio className="clip" id="6" src={audioSource(6)}></audio>
+                            </div>
                         </div>
-                        <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="D">
-                            <span className='fs-1'>D</span>
-                            <audio className="clip" id="6" src={audioSource(6)}></audio>
+                        <div className="row">
+                            <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="Z">
+                                <span className='fs-1'>Z</span>
+                                <audio className="clip" id="7" src={audioSource(7)}></audio>
+                            </div>
+                            <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="X">
+                                <span className='fs-1'>X</span>
+                                <audio className="clip" id="8" src={audioSource(8)}></audio>
+                            </div>
+                            <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="C">
+                                <span className='fs-1'>C</span>
+                                <audio className="clip" id="9" src={audioSource(9)}></audio>
+                            </div>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="Z">
-                            <span className='fs-1'>Z</span>
-                            <audio className="clip" id="7" src={audioSource(7)}></audio>
-                        </div>
-                        <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="X">
-                            <span className='fs-1'>X</span>
-                            <audio className="clip" id="8" src={audioSource(8)}></audio>
-                        </div>
-                        <div className="col drum-pad btn btn-primary m-1" onClick={handleClick} id="C">
-                            <span className='fs-1'>C</span>
-                            <audio className="clip" id="9" src={audioSource(9)}></audio>
-                        </div>
-                    </div>
-                </div>
-            </div></div>
-    );
-};
+                </div></div>
+        );
+    };
 
 
-export default DrumMachine;
+    export default DrumMachine;
